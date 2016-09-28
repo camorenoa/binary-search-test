679907e4ade9ab84711024253c0997889e161c6d
César Moreno Amado <camorenoa@yahoo.es>
2016-09-28 17:22:35 -0500

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -4,7 +4,7 @@
 
 var numbersArray = function() {
   var current = 0;
-  var top = 1000;
+  var top = 100000;
   var array = [];
   if(top)  {
     while(--top) {
@@ -15,7 +15,8 @@ var numbersArray = function() {
   return array;
 };
 // console.log(numbersArray());
-
+var date = new Date();
+var totalTime = 0;
 function binarySearch(array, min, max, searchNum) {
   var half = Math.floor((max + min)/2);
   console.log('min-max', min, max, half, array[half]);
@@ -28,10 +29,12 @@ function binarySearch(array, min, max, searchNum) {
     max = half - 1;
     binarySearch(array, min, max, searchNum);
   } else if (array[half] === searchNum) {
-    console.log('num', searchNum);
+    var newDate = new Date();
+    totalTime = newDate - date;
+    console.log('num', searchNum, totalTime);
     return searchNum;
   }
 }
 var arrayNumbers = numbersArray().sort(function(a, b){return a - b;});
 console.log(arrayNumbers);
-binarySearch(arrayNumbers, 0, arrayNumbers.length - 1, 280);
+binarySearch(arrayNumbers, 0, arrayNumbers.length - 1, 1000);
